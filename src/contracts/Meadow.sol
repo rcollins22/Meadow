a892f1ea5ce29c300383d4a6488c92f4e2647ef1
Rashad Collins <rashad.collins22@gmail.com>
2020-12-16 20:10:30 -0500

    deploy test

--- src/contracts/Meadow.sol
+++ src/contracts/Meadow.sol
@@ -1,30 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract Meadow {
-  string public name = "appname";
+  string public name = "Meadow";
 
-  uint public idCount = 0;
-  mapping(uint => Photo) public photos;
   
-  struct Photo {
-    unit id;
-    string hash;
-    string description;
-    string author;
-  }
-
-  event PhotoPosted(
-    uint id,
-    string hash,
-    string description,
-    string author,
-  )
-
-  function post(string memory _imgHash, string memory _description) public {
-    require(bytes(_imgHash).length >0);
-    require(msg.sender != address(0x0));
-    
-    idCount ++ ;
-    photos[idCount] = Photo(idCount, _imgHash,'_description, msg.sender)
-  }
 }
