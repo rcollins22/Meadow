5f1e071683cefbc1b72fcadd7bd9936b41a0c625
Rashad Collins <rashad.collins22@gmail.com>
2020-12-16 21:27:59 -0500

    test

--- src/contracts/Meadow.sol
+++ src/contracts/Meadow.sol
@@ -2,6 +2,20 @@ pragma solidity ^0.5.0;
 
 contract Meadow {
   string public name = "Meadow";
-
   
+  uint public idCount = 0 ;
+  mapping(uint => Photo) public photos;
+
+  struct Photo {
+    uint id;
+    string hash;
+    string descripton;
+    address author;
+  }
+
+  function post(string memory iHash, string memory desc) public {
+    
+    idCount ++;
+    photos[idCount]= Photo(idCount,iHash,desc,msg.sender)
+  }
 }
--- test/test.js
+++ test/test.js
@@ -1,3 +1,29 @@
+const { assert } = require("chai");
+
 const Meadow = artifacts.require("./Meadow.sol");
 
 require("chai").use(require("chai-as-promised")).should();
+
+contract('Meadow',([deployer,author])=>{
+  let meadow
+
+  before(async () => {
+    meadow = await Meadow.deployed()
+  })
+
+  describe('Is the contract on the Blockchain?', async () => {
+    it('successfully deployed', async ()=> {
+      const address = await meadow.address
+      assert.notEqual(address, 0x0)
+      assert.notEqual(address,"")
+      assert.notEqual(address, undefined)
+      assert.notEqual(address, null)
+    })
+    it("Name is Set", async ()=> {
+      const name = await meadow.name()
+      assert.equal(name,"Meadow")
+    })
+  })
+
+  
+})
